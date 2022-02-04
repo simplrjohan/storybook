07f61b11cba82d9b187377008c29186ede9f9180
simplrjohan <johan@simplr.ai>
2022-02-04 10:33:15 -0600

    add files

---
+++ dist/esm/index.js
@@ -0,0 +1,16 @@
+import n from"react";import r from"styled-components";
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+***************************************************************************** */var e=function(){return e=Object.assign||function(n){for(var r,e=1,o=arguments.length;e<o;e++)for(var t in r=arguments[e])Object.prototype.hasOwnProperty.call(r,t)&&(n[t]=r[t]);return n},e.apply(this,arguments)};var o,t,i,p=r.button(i||(o=["\n    border: 0;\n    line-height: 1;\n    font-size: 15px;\n    cursor: pointer;\n    font-weight: 700;\n    font-weight: bold;\n    border-radius: 3px;\n    display: inline-block;\n    padding: ",";\n    color: ",";\n    background-color: ",";\n    opacity: ",";\n    &:hover {\n      background-color: ",";\n    }\n    &:active {\n        border: solid 2px #1b116e;\n        padding: ",";\n    }\n"],t=["\n    border: 0;\n    line-height: 1;\n    font-size: 15px;\n    cursor: pointer;\n    font-weight: 700;\n    font-weight: bold;\n    border-radius: 3px;\n    display: inline-block;\n    padding: ",";\n    color: ",";\n    background-color: ",";\n    opacity: ",";\n    &:hover {\n      background-color: ",";\n    }\n    &:active {\n        border: solid 2px #1b116e;\n        padding: ",";\n    }\n"],Object.defineProperty?Object.defineProperty(o,"raw",{value:t}):o.raw=t,i=o),(function(n){return"small"===n.size?"7px 25px 8px":"medium"===n.size?"9px 30px 11px":"14px 30px 16px"}),(function(n){return n.primary?"#1b116e":"#ffffff"}),(function(n){return n.primary?"#6bedb5":"#1b116e"}),(function(n){return n.disabled?.5:1}),(function(n){return n.primary?"#55bd90":"#6bedb5"}),(function(n){return"small"===n.size?"5px 23px 6px":"medium"===n.size?"7px 28px 9px":"12px 28px 14px"})),a=function(r){var o=r.size,t=r.primary,i=r.disabled,a=r.text,c=r.onClick,l=function(n,r){var e={};for(var o in n)Object.prototype.hasOwnProperty.call(n,o)&&r.indexOf(o)<0&&(e[o]=n[o]);if(null!=n&&"function"==typeof Object.getOwnPropertySymbols){var t=0;for(o=Object.getOwnPropertySymbols(n);t<o.length;t++)r.indexOf(o[t])<0&&Object.prototype.propertyIsEnumerable.call(n,o[t])&&(e[o[t]]=n[o[t]])}return e}(r,["size","primary","disabled","text","onClick"]);return n.createElement(p,e({type:"button",onClick:c,primary:t,disabled:i,size:o},l),a)};export{a as Button};
+//# sourceMappingURL=index.js.map
---
+++ dist/index.d.ts
@@ -0,0 +1,13 @@
+import { MouseEventHandler, FC } from 'react';
+
+interface ButtonProps {
+    text?: string;
+    primary?: boolean;
+    disabled?: boolean;
+    size?: "small" | "medium" | "large";
+    onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+declare const Button: FC<ButtonProps>;
+
+export { Button };
